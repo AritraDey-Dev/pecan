38597cb0ae2038e6d5ea82719872d7a6e9bb9649
Aritra Dey <adey01027@gmail.com>
2025-03-14 15:21:00 +0530

    refractor: available-models.R

--- apps/api/R/available-models.R
+++ apps/api/R/available-models.R
@@ -1,5 +1,40 @@
 library(magrittr, include.only = "%>%")
 
+#' Get machine ID from hostname
+get_machine_id <- function(machine_name) {
+  machine_id <- dplyr::tbl(global_db_pool, "machines") %>%
+    dplyr::filter(hostname == !!machine_name) %>%
+    dplyr::pull(id)
+
+  if (length(machine_id) > 1) {
+    stop("Found ", length(machine_id), " machines with name ", machine_name)
+  }
+  if (length(machine_id) < 1) {
+    stop("Found no machines with name ", machine_name)
+  }
+  
+  return(machine_id)
+}
+
+#' Get model-related files from database
+get_model_files <- function(machine_id) {
+  dplyr::tbl(global_db_pool, "dbfiles") %>%
+    dplyr::filter(machine_id == !!machine_id, container_type == "Model")
+}
+
+#' Combine model files with models and model types
+combine_model_data <- function(modelfiles) {
+  models <- dplyr::tbl(global_db_pool, "models")
+  modeltypes <- dplyr::tbl(global_db_pool, "modeltypes") %>%
+    dplyr::select(modeltype_id = id, modeltype = name)
+
+  modelfiles %>%
+    dplyr::select(dbfile_id = id, file_name, file_path, model_id = container_id) %>%
+    dplyr::inner_join(models, c("model_id" = "id")) %>%
+    dplyr::inner_join(modeltypes, "modeltype_id") %>%
+    dplyr::collect()
+}
+
 #' List models available on a specific machine
 #'
 #' @param machine_name Target machine hostname. Default = `"docker"`
@@ -9,29 +44,9 @@ library(magrittr, include.only = "%>%")
 #* @get /
 availableModels <- function(machine_name = "docker", machine_id = NA) {
   if (is.na(machine_id)) {
-    machines <- dplyr::tbl(global_db_pool, "machines")
-    machineid <- machines %>%
-      dplyr::filter(hostname == !!machine_name) %>%
-      dplyr::pull(id)
-    if (length(machineid) > 1) {
-      stop("Found ", length(machineid), " machines with name ", machine_name)
-    }
-    if (length(machineid) < 1) {
-      stop("Found no machines with name ", machine_name)
-    }
+    machine_id <- get_machine_id(machine_name)
   }
-  dbfiles <- dplyr::tbl(global_db_pool, "dbfiles") %>%
-    dplyr::filter(machine_id == !!machineid)
-  modelfiles <- dbfiles %>%
-    dplyr::filter(container_type == "Model")
-  models <- dplyr::tbl(global_db_pool, "models")
-  modeltypes <- dplyr::tbl(global_db_pool, "modeltypes") %>%
-    dplyr::select(modeltype_id = id, modeltype = name)
 
-  modelfiles %>%
-    dplyr::select(dbfile_id = id, file_name, file_path,
-                  model_id = container_id) %>%
-    dplyr::inner_join(models, c("model_id" = "id")) %>%
-    dplyr::inner_join(modeltypes, "modeltype_id") %>%
-    dplyr::collect()
+  modelfiles <- get_model_files(machine_id)
+  combine_model_data(modelfiles)
 }
