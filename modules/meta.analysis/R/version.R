84d87f0ae7ab1fb006ae9b27ae1251542bb706b5
Aritra Dey <adey01027@gmail.com>
2025-12-12 21:13:10 +0530

    test

--- modules/meta.analysis/R/version.R
+++ modules/meta.analysis/R/version.R
@@ -1,3 +1,5 @@
 # Set at package install time, used by pecan.all::pecan_version()
 # to identify development versions of packages
 .build_hash <- Sys.getenv("PECAN_GIT_REV", "unknown")
+
+# Test comment to verify workflow optimization
